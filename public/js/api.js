cd806559cde0d33aa7828d64eae09b9c35e3fb5f
ladynazlia <amandalfwrites@gmail.com>
2022-09-27 22:07:41 -0400

    after lots of trial and error, fixed the JS Script in the profiles

---
+++ public/js/api.js
@@ -0,0 +1,25 @@
+document.querySelector('#infoBtnClick').addEventListener('click', getInfo)
+
+function getInfo() {
+    let select = document.getElementById('learningDisability')
+    let learningDisability = select.value
+    
+    console.log(learningDisability)
+
+
+
+//     // const URL = 'https://accommodations-api.onrender.com/api/'+ learningDisability
+//     // fetch(url) 
+//     //     .then(res => res.json())
+//     //     .then(data => {
+//     //         console.log(data)
+//     //     })
+//     //     .catch(err => {
+//     //         console.log(`error ${err}`)
+//     //     });
+}
+
+
+
+console.log('hi')
+
