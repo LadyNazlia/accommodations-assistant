22ba86bceb529bd74bae268d33224938b6d0b4d2
ladynazlia <amandalfwrites@gmail.com>
2022-09-28 20:22:30 -0400

    Working on updating DOM with data from API

--- public/js/api.js
+++ public/js/api.js
@@ -3,18 +3,34 @@ document.querySelector('#infoBtnClick').addEventListener('click', getInfo)
 function getInfo() {
     let select = document.getElementById('learningDisability')
     let learningDisability = select.value
-    
-    console.log(learningDisability)
+    let disabilityTrouble = document.querySelector('#disabilityTrouble')
+
+
 
     const url = 'https://accommodations-api.onrender.com/api/'+ learningDisability
     fetch(url) 
         .then(res => res.json())
         .then(data => {
-            console.log(data.disability)
-            console.log(data.explanation)
-            console.log(data.important)
-            console.log(data.troubleAreas) //doesn't work atm - undefined
-             console.log(data.accommodation)
+            //clears ul in case user doesn't refresh page between button clicks
+            disabilityTrouble.innerHTML = ''
+            document.querySelector('#disabilityName').innerHTML = data.disability
+            document.querySelector('#ExplanationLabel').innerHTML = "Explanation"
+            document.querySelector('#disabilityExplanation').innerHTML = data.explanation
+            document.querySelector('#importantLabel').innerHTML = "IMPORTANT!"
+            document.querySelector('#disabilityImportant').innerHTML = data.important
+            document.querySelector('#troubleLabel').innerHTML = "Trouble Areas:"
+            
+
+            for (let troubleAreas in learningDisability) {
+                let value = troubleAreas;
+                listItem = document.createElement("li");
+                listItem.innerHTML = value;
+                disabilityTrouble.appendChild(listItem);
+              }
+
+            //document.querySelector('#disabilityTrouble').innerHTML = data.troubleAreas
+            // document.querySelector('#disabilityAccommodation').innerHTML = data.accommodation
+            console.log(data.troubleAreas)
 
         })
         .catch(err => {
