ba05973856e6a537b97055b219506f1f4545aa00
ladynazlia <amandalfwrites@gmail.com>
2022-09-30 19:19:45 -0400

    Fixed API DOM display for lists

--- public/js/api.js
+++ public/js/api.js
@@ -4,7 +4,7 @@ function getInfo() {
     let select = document.getElementById('learningDisability')
     let learningDisability = select.value
     let disabilityTrouble = document.querySelector('#disabilityTrouble')
-
+    
 
 
     const url = 'https://accommodations-api.onrender.com/api/'+ learningDisability
@@ -12,33 +12,30 @@ function getInfo() {
         .then(res => res.json())
         .then(data => {
             //clears ul in case user doesn't refresh page between button clicks
-            disabilityTrouble.innerHTML = ''
+            disabilityTrouble.innerHTML = ' '
             document.querySelector('#disabilityName').innerHTML = data.disability
             document.querySelector('#ExplanationLabel').innerHTML = "Explanation"
             document.querySelector('#disabilityExplanation').innerHTML = data.explanation
             document.querySelector('#importantLabel').innerHTML = "IMPORTANT!"
             document.querySelector('#disabilityImportant').innerHTML = data.important
-            document.querySelector('#troubleLabel').innerHTML = "Trouble Areas:"
+            document.querySelector('#troubleLabel').innerHTML = "Trouble Areas:";
             
+        
+                for (let i = 0; i < data.troubleAreas.length; i++) {
+                let li = document.createElement('li') 
+                li.innerHTML = data.troubleAreas[i]
+                disabilityTrouble.appendChild(li)
+                }
+            document.querySelector('#accommodationLabel').innerHTML = "Recommended Accommodations:";
 
-            for (let troubleAreas in learningDisability) {
-                let value = troubleAreas;
-                listItem = document.createElement("li");
-                listItem.innerHTML = value;
-                disabilityTrouble.appendChild(listItem);
-              }
-
-            //document.querySelector('#disabilityTrouble').innerHTML = data.troubleAreas
-            // document.querySelector('#disabilityAccommodation').innerHTML = data.accommodation
-            console.log(data.troubleAreas)
+                for (let i = 0; i < data.accommodation.length; i++) {
+                    let li = document.createElement('li') 
+                    li.innerHTML = data.accommodation[i]
+                    accommodations.appendChild(li)
+                }
 
         })
         .catch(err => {
             console.log(`error ${err}`)
-        });
-}
-
-
-
-console.log('hi')
-
+        })
+    }
